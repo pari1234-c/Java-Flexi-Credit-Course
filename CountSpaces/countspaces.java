8b0785daf2ce3982b055dcddc56121b75636771d
pari1234-c <parinitachoudhari850@gmail.com>
2026-04-21 12:22:01 +0530

    Add CountSpaces program to count spaces in a string
    This program counts the number of spaces in a given string.

---
+++ CountSpaces/countspaces.java
@@ -0,0 +1,28 @@
+public class CountSpaces {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        String str = "Symbiosis Institute of Technology";
+
+        System.out.println(str);
+
+        int len = str.length();
+        System.out.println(len);
+
+        String s1 = str.toUpperCase();
+        System.out.println(s1);
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            char ch = s1.charAt(i);
+
+            if (ch == ' ') {
+                count++;
+            }
+        }
+
+        System.out.println("Count of spaces is: " + count);
+    }
+}
