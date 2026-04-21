8e43b49ccc7bc3dff2c4175dd44168476e92a8b6
pari1234-c <parinitachoudhari850@gmail.com>
2026-04-21 10:44:33 +0530

    Add StringOperations class for string manipulation

---
+++ StringOperations/stringoperations.java
@@ -0,0 +1,18 @@
+public class StringOperations {
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        System.out.println(str);
+
+        int len = str.length();
+        System.out.println(len);
+
+        String s1 = str.toUpperCase();
+        System.out.println(s1);
+
+        String s2 = str.toLowerCase();
+        System.out.println(s2);
+    }
+}
