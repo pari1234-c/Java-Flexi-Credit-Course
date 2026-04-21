32e85c2b7b185d9787954c6bd012526311cd8c5c
pari1234-c <parinitachoudhari850@gmail.com>
2026-04-21 11:02:05 +0530

    Add Student class with properties and methods

---
+++ Student/student.java
@@ -0,0 +1,30 @@
+public class Student {
+
+    private int stdPrn;
+    private String stdName;
+    private String stdDept;
+
+    public int getStdPrn() {
+        return stdPrn;
+    }
+
+    public void setStdPrn(int stdPrn) {
+        this.stdPrn = stdPrn;
+    }
+
+    public String getStdName() {
+        return stdName;
+    }
+
+    public void setStdName(String stdName) {
+        this.stdName = stdName;
+    }
+
+    public String getStdDept() {
+        return stdDept;
+    }
+
+    public void setStdDept(String stdDept) {
+        this.stdDept = stdDept;
+    }
+}
