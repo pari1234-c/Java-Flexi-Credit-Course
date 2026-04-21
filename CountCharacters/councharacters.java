4e08124002f0662398a897f52ccf61fe4cc43481
pari1234-c <parinitachoudhari850@gmail.com>
2026-04-21 12:25:10 +0530

    Create councharacters.java

---
+++ CountCharacters/councharacters.java
@@ -0,0 +1,28 @@
+public class CountCharacterS {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        String str = "Symbiosis";
+
+        System.out.println(str);
+
+        int len = str.length();
+        System.out.println(len);
+
+        String s1 = str.toUpperCase();
+        System.out.println(s1);
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            char ch = s1.charAt(i);
+
+            if (ch == 'S') {
+                count++;
+            }
+        }
+
+        System.out.println("Count of S is: " + count);
+    }
+}
